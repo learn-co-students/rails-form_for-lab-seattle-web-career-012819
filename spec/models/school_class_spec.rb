12160292de981b121a22df9c3425ce28797e0c11
github username <github email address>
2019-02-25 20:01:17 -0800

    Done.

--- spec/models/school_class_spec.rb
+++ spec/models/school_class_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
+
 describe SchoolClass do
   before(:each) do
     @school_class = SchoolClass.create!(title: "Computer Science", room_number: 5)
